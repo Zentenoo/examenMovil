d1aff63e9ce4a3345846f08699bf3c179fcd91fa
Mateo <94015253+Zentenoo@users.noreply.github.com>
2023-05-03 14:23:57 -0400

    Update InicioScreen.js

--- examen/screens/InicioScreen.js
+++ examen/screens/InicioScreen.js
@@ -1,32 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList,Button } from 'react-native';
 
-export function InicioScreen({ navigation }) {
+export function InicioScreen ({ navigation, route }){
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      const notesFromStorage = JSON.parse(localStorage.getItem('notes') || '[]');
-  setNotes(notesFromStorage);
-    },[]);
+    const getNotes = async () => {
+      const notesFromStorage = JSON.parse(
+        localStorage.getItem('notes') || '[]'
+      );
+      setNotes(notesFromStorage);
+    };
 
-    return unsubscribe;
-  }, [navigation]);
+    getNotes();
+  }, []);
+
+  useEffect(() => {
+    if (route.params?.setNotes) {
+      const updatedNotes = JSON.parse(localStorage.getItem('notes') || '[]');
+      setNotes(updatedNotes);
+      route.params.setNotes(updatedNotes);
+    }
+  }, [route.params?.setNotes]);
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>Lista de notas</Text>
-      {notes.length > 0 ? (
-        notes.map((note, index) => (
-          <View key={index} style={{ marginBottom: 10 }}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{note.title}</Text>
-            <Text style={{ fontSize: 16 }}>{note.content}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista de Tareas</Text>
+      <FlatList
+        data={notes}
+        keyExtractor={(note) => note.id}
+        renderItem={({ item }) => (
+          <View style={styles.note}>
+            <Text style={styles.noteTitle}>{item.title}</Text>
+            <Text style={styles.noteContent}>{item.content}</Text>
+            <Text style={styles.noteDate}>{item.date}</Text>
           </View>
-        ))
-      ) : (
-        <Text style={{ fontSize: 18 }}>No hay notas guardadas</Text>
-      )}
+        )}
+      />
       <Button title="Agregar nota" onPress={() => navigation.navigate('Tareas', { setNotes })} />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  note: {
+    marginBottom: 20,
+  },
+  noteTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  noteContent: {
+    fontSize: 16,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  noteDate: {
+    fontSize: 14,
+    color: '#666',
+  },
+});
